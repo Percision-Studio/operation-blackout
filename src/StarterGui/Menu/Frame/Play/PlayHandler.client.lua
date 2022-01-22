f6448e7244457cce71394fb56f5853455f51645b
BossFreind1234 <vincentredtube@gmail.com>
2022-01-22 11:17:52 -0500

    Moved all of the maps and game modes to places

--- src/StarterGui/Menu/Frame/Play/PlayHandler.client.lua
+++ src/StarterGui/Menu/Frame/Play/PlayHandler.client.lua
@@ -1,5 +1,5 @@
 script.Parent.Parent.GameMenu.Casual.MouseButton1Click:connect(function()
  
-    game:GetService("TeleportService"):Teleport("8579057043")
+    game:GetService("TeleportService"):Teleport("8611754466")
  
 end)
